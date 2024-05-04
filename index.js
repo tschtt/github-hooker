80db0b1caabf920dd6061e822cf5357f7098ca69
Santiago Taschetta <santiagotaschetta@gmail.com>
2024-05-04 20:55:44 -0300

    execute script on event recieved

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { createServer } from 'node:http'
+import { exec } from 'child_process';
 
 const PORT = process.env.PORT;
+const SECRET = process.env.SECRET;
 
 async function handler (request, response) {
     const { headers, method, url } = request;
@@ -17,8 +19,30 @@ async function handler (request, response) {
     request.on('end', () => {
         body = Buffer.concat(body).toString();
         console.error({ at: Date.now(), headers, method, url, body });
+        
         response.writeHead(200);
         response.end("Heard that!");
+
+        try {
+            body = JSON.parse(body)
+            
+            if(body.hook.config.secret !== SECRET) {
+                throw new Error(`The secret sent is incorrect...`)
+            }
+
+            if(body.repository.name === 'segundo-subsuelo') {
+                exec('bash ~/scripts/build-segundo-subsuelo.sh', (error, stdout, stderr) => {
+                    console.log(stdout);
+                    console.log(stderr);
+                    if (error !== null) {
+                        console.log(`exec error: ${error}`);
+                    }
+                })
+            }
+            
+        } catch (error) {
+            console.error(error)
+        }
     });
 }
 
